42809473e789c07fadf544c25deb70fba104bb7d
MyBug18 <mskimkim0318@gmail.com>
2020-07-09 11:52:50 +0900

    Restored HexTileCoord struct

--- Assets/Scripts/Core/TileInfo.cs
+++ Assets/Scripts/Core/TileInfo.cs
@@ -3,6 +3,31 @@ using Infinity.Core.Modifier;
 
 namespace Infinity.Core
 {
+    /// <summary>
+    /// Clockwise tile direction
+    /// </summary>
+    public enum TileDirection
+    {
+        Up,        // (+1,  0)
+        UpRight,   // (+1, -1)
+        DownRight, // ( 0, -1)
+        Down,      // (-1,  0)
+        DownLeft,  // (-1, +1)
+        UpLeft,    // ( 0, +1)
+    }
+
+    public struct HexTileCoord
+    {
+        public readonly int q;
+        public readonly int r;
+
+        public HexTileCoord(int q, int r)
+        {
+            this.q = q;
+            this.r = r;
+        }
+    }
+
     public class TileInfo : IModifierAttachable
     {
         public readonly HexTileCoord Coord;
