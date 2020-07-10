89a4f9f68aeb4d8514d3fa240524396056f181b7
MyBug18 <mskimkim0318@gmail.com>
2020-07-10 10:22:00 +0900

    Renamed MessageHandler

---
+++ Assets/Scripts/Infinity/EventHandler.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Infinity
+{
+    public delegate bool SubscribeCallback(Event e);
+
+    public abstract class Event : IDisposable
+    {
+        public abstract void Dispose();
+    }
+
+    public readonly struct EventInfo
+    {
+        public readonly SubscribeCallback Callback;
+
+        public readonly bool ReceiveOnlyOnce;
+
+        public EventInfo(SubscribeCallback c, bool once)
+        {
+            Callback = c;
+            ReceiveOnlyOnce = once;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is SubscribeCallback c && Equals(c);
+        }
+
+        public override int GetHashCode()
+        {
+            return Callback.GetHashCode();
+        }
+    }
+
+    /// <summary>
+    /// Super simple event handler
+    /// </summary>
+    public class EventHandler
+    {
+        private readonly Dictionary<Type, List<EventInfo>> subscribeInfoDict = new Dictionary<Type, List<EventInfo>>();
+
+        public void Subscribe<T>(SubscribeCallback c, bool once = false) where T : Event
+        {
+            var type = typeof(T);
+            var eventInfo = new EventInfo(c, once);
+
+            // Make new list and add it when there are no initial list for the given type
+            if (!subscribeInfoDict.ContainsKey(type))
+            {
+                var list = new List<EventInfo>();
+                subscribeInfoDict.Add(type, list);
+            }
+
+            subscribeInfoDict[type].Add(eventInfo);
+        }
+
+        public void UnSubscribe<T>(SubscribeCallback c) where T : Event
+        {
+            var type = typeof(T);
+
+            if (subscribeInfoDict.TryGetValue(type, out var infos))
+            {
+                var idx = infos.FindIndex(i => i.Callback.Equals(c));
+                if (idx == -1) return;
+
+                infos.RemoveAt(idx);
+                if (infos.Count == 0)
+                    subscribeInfoDict.Remove(type);
+            }
+        }
+
+        public void Publish<T>(T e) where T : Event
+        {
+            var type = typeof(T);
+            var removeList = new List<EventInfo>();
+
+            if (!subscribeInfoDict.TryGetValue(type, out var infos)) return;
+
+            foreach (var i in infos)
+            {
+                i.Callback.Invoke(e);
+
+                if (i.ReceiveOnlyOnce)
+                    removeList.Add(i);
+            }
+
+            foreach (var i in removeList)
+                infos.Remove(i);
+
+            if (infos.Count == 0)
+                subscribeInfoDict.Remove(type);
+
+            e.Dispose();
+        }
+    }
+}
