556ace73826385e07ec3f46e4d269248b8320df1
MyBug18 <mskimkim0318@gmail.com>
2020-07-10 11:22:50 +0900

    Added planet tester

---
+++ Assets/Scripts/Tester/PlanetTester.cs
@@ -0,0 +1,20 @@
+﻿using Infinity.HexTileMap;
+using Infinity.Planet;
+using UnityEngine;
+
+namespace Tester
+{
+    public class PlanetTester : MonoBehaviour
+    {
+        [SerializeField]
+        private PlanetWrapper planetPrefab;
+
+        private void Start()
+        {
+            var planet = new Planet(new HexTileCoord(0, 0), "TestEarth", 3);
+
+            var p = Instantiate(planetPrefab, transform);
+            p.Init(planet);
+        }
+    }
+}
