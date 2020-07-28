12054cee9c607e5c9e83b14169a849600c475d4a
MyBug18 <mskimkim0318@gmail.com>
2020-07-28 16:03:10 +0900

    Added basic GameEventSignal

---
+++ Assets/Scripts/Infinity/GameEventSignal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Infinity
+{
+    public class GameEventSignal<TTarget> : ISignal where TTarget : ISignalDispatcherHolder
+    {
+        public ISignalDispatcherHolder SignalSender { get; }
+
+        public string Command { get; }
+    }
+}
