f33fb265ccd68061f3295142b9267fa805781961
MyBug18 <mskimkim0318@gmail.com>
2020-07-30 14:35:20 +0900

    Refined BuildingPrototype loading logic

--- Assets/Scripts/Infinity/PlanetPop/BuildingCore/BuildingPrototype.cs
+++ Assets/Scripts/Infinity/PlanetPop/BuildingCore/BuildingPrototype.cs
@@ -13,8 +13,9 @@ namespace Infinity.PlanetPop.BuildingCore
 
         public readonly int BaseConstructCost;
 
-        private readonly List<PopSlotPrototype> _slots;
+        private readonly List<PopSlotPrototype> _slots = new List<PopSlotPrototype>();
         public IReadOnlyList<PopSlotPrototype> Slots => _slots;
+
 
         public Func<Planet, bool> ConditionChecker { get; private set; }
 
@@ -23,20 +24,36 @@ namespace Infinity.PlanetPop.BuildingCore
             var primary = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonData);
 
             Name = (string)primary["Name"];
-            BaseConstructTime = (int)(long)primary["BaseConstructTime"];
-            BaseConstructCost = (int)(long)primary["BaseConstructCost"];
+            BaseConstructTime = Convert.ToInt32(primary["BaseConstructTime"]);
+            BaseConstructCost = Convert.ToInt32(primary["BaseConstructCost"]);
 
-            _slots = JArray.FromObject(primary["Slots"]).ToObject<List<PopSlotPrototype>>();
+            foreach (var slotData in JArray.FromObject(primary["Slots"]))
+            {
+                _slots.Add(new PopSlotPrototype(slotData.ToString()));
+            }
+
+            ConditionChecker = p => p.Name == "asdf";
         }
     }
 
-    public struct PopSlotPrototype
+    public class PopSlotPrototype
     {
-        public List<FactorChangePrototype> Yield;
+        private readonly List<FactorChangePrototype> _yield;
+        public IReadOnlyList<FactorChangePrototype> Yield => _yield;
 
-        public List<FactorChangePrototype> Upkeep;
+        private readonly List<FactorChangePrototype> _upkeep;
+        public IReadOnlyList<FactorChangePrototype> Upkeep => _upkeep;
 
-        public float Wage;
+        public readonly float Wage;
+
+        public PopSlotPrototype(string jsonData)
+        {
+            var primary = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonData);
+
+            _yield = JArray.FromObject(primary["Yield"]).ToObject<List<FactorChangePrototype>>();
+            _upkeep = JArray.FromObject(primary["Upkeep"]).ToObject<List<FactorChangePrototype>>();
+            Wage = Convert.ToSingle(primary["Wage"]);
+        }
     }
 
     public struct FactorChangePrototype
