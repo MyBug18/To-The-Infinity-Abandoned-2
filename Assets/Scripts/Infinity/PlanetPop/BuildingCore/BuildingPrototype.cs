64ce8d6e5b65e2980b1208d52c782dee75e92e08
MyBug18 <mskimkim0318@gmail.com>
2020-07-30 18:38:21 +0900

    Added string tokenizer

--- Assets/Scripts/Infinity/PlanetPop/BuildingCore/BuildingPrototype.cs
+++ Assets/Scripts/Infinity/PlanetPop/BuildingCore/BuildingPrototype.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Infinity.HexTileMap;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -7,6 +11,16 @@ namespace Infinity.PlanetPop.BuildingCore
 {
     public class BuildingPrototype
     {
+        private enum TokenType
+        {
+            Parenthesis,
+            Not,
+            Equal,
+            And,
+            Or,
+            Word
+        }
+
         public readonly string Name;
 
         public readonly int BaseConstructTime;
@@ -16,8 +30,10 @@ namespace Infinity.PlanetPop.BuildingCore
         private readonly List<PopSlotPrototype> _slots = new List<PopSlotPrototype>();
         public IReadOnlyList<PopSlotPrototype> Slots => _slots;
 
-        [JsonIgnore]
-        public Func<Planet, bool> ConditionChecker { get; private set; }
+        private readonly Dictionary<string, object> _conditions;
+        public IReadOnlyDictionary<string, object> Conditions => _conditions;
+
+        private readonly Func<(Planet, HexTileCoord), bool> _conditionChecker;
 
         public BuildingPrototype(string jsonData)
         {
@@ -32,8 +48,84 @@ namespace Infinity.PlanetPop.BuildingCore
                 _slots.Add(new PopSlotPrototype(slotData.ToString()));
             }
 
-            ConditionChecker = p => p.Name == "asdf";
+            _conditions = JObject.FromObject(primary["Condition"]).ToObject<Dictionary<string, object>>();
+            if (_conditions == null) return;
+
+            ConstructConditionChecker();
         }
+
+        public List<HexTileCoord> GetBuildableTile(Planet planet)
+        {
+            return null;
+        }
+
+        private void ConstructConditionChecker()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var kv in _conditions)
+            {
+                switch (kv.Key)
+                {
+                    case "MinPopNumber":
+                        sb.Append($"{kv.Key} <= Item1.Pops.Count && ");
+                        break;
+                    case "TileState":
+                        var tokens = TokenizeConditionString(Convert.ToString(kv.Value));
+                        break;
+                }
+            }
+        }
+
+        private List<(string, TokenType)> TokenizeConditionString(string condition)
+        {
+            var result = new List<(string, TokenType)>();
+            var currentWordToken = new List<char>();
+
+            foreach (var c in condition)
+            {
+                if ('a' <= c && c <= 'z' || 'A' <= c && c <= 'Z')
+                {
+                    currentWordToken.Add(c);
+                    continue;
+                }
+
+                if (!IsValidSpecialCharacter(c))
+                    throw new InvalidOperationException($"Unexpected character : {c} in {Name}.");
+
+                if (currentWordToken.Count > 0)
+                {
+                    result.Add((new string(currentWordToken.ToArray()), TokenType.Word));
+                    currentWordToken.Clear();
+                }
+
+                switch (c)
+                {
+                    case '(':
+                    case ')':
+                        result.Add(($"{c}", TokenType.Parenthesis));
+                        break;
+                    case '!':
+                        result.Add(("!", TokenType.Not));
+                        break;
+                    case '=':
+                        result.Add(("=", TokenType.Equal));
+                        break;
+                    case '&':
+                        result.Add(("&", TokenType.And));
+                        break;
+                    case '|':
+                        result.Add(("|", TokenType.Or));
+                        break;
+                    case ' ':
+                        break;
+                }
+            }
+
+            return result;
+        }
+
+        private bool IsValidSpecialCharacter(char c) => " !&|()=".Contains(c);
     }
 
     public class PopSlotPrototype
