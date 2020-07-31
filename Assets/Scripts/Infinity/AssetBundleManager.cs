058de9027394e2ba70ec5ab57d26f8fe2329e8f6
MyBug18 <mskimkim0318@gmail.com>
2020-07-31 10:42:39 +0900

    Changed AssetBundleManager to GameDataStorage

--- Assets/Scripts/Infinity/AssetBundleManager.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Infinity
-{
-    public class AssetBundleManager
-    {
-        private static AssetBundleManager _instance;
-
-        public static AssetBundleManager Instance => _instance ??= new AssetBundleManager();
-    }
-}
---
+++ Assets/Scripts/Infinity/GameDataStorage.cs
@@ -0,0 +1,9 @@
+﻿namespace Infinity
+{
+    public class GameDataStorage
+    {
+        private static GameDataStorage _instance;
+
+        public static GameDataStorage Instance => _instance ??= new GameDataStorage();
+    }
+}
