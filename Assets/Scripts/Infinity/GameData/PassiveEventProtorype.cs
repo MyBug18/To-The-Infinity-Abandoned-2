9c8d11a6f53ac9017dc09ae2e0a879c86429bf22
MyBug18 <mskimkim0318@gmail.com>
2020-08-06 15:21:00 +0900

    Added basic PassiveEventPrototype

---
+++ Assets/Scripts/Infinity/GameData/PassiveEventProtorype.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Infinity.GameData
+{
+    public class PassiveEventProtorype
+    {
+        public readonly string Name;
+
+        public readonly string EventOwnerType;
+
+        public readonly IPropositionalLogic<Game> GameConditionChecker;
+    }
+}
